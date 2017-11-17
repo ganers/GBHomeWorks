088cce7878bfba126f4116fca51da95300e7debb
ganers <ganers@mail.ru>
2017-11-17 21:26:53 +0500

    2

--- cshlvl1/Program.cs
+++ cshlvl1/Program.cs
@@ -7,49 +7,49 @@ using System.Xml.Linq;
 
 namespace cshlvl1
 {
-    class User
-    {
-        //public static FName {get;set};
+    //class User
+    //{
+    //    //public static FName {get;set};
 
-    static List<User> LoadXml(string path)
-    {
-        string dataXml = System.IO.File.ReadAllText(path);
+    //static List<User> LoadXml(string path)
+    //{
+    //    string dataXml = System.IO.File.ReadAllText(path);
 
-        var tempCol = XDocument.Parse(dataXml).Descendants("Dep");
+    //    var tempCol = XDocument.Parse(dataXml).Descendants("Dep");
 
-        foreach (var item in tempCol)
-        {
-            Console.WriteLine(item);
-        }
-        return null;
-    }
+    //    foreach (var item in tempCol)
+    //    {
+    //        Console.WriteLine(item);
+    //    }
+    //    return null;
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-            CreateXML();
+            //CreateXML();
         }
 
-        static void CreateXML()
-        {
-            XElement _dep = new XElement("Dep");
+        //static void CreateXML()
+        //{
+        //    XElement _dep = new XElement("Dep");
 
-            for (int i = 0; i < 10; i++)
-            {
-                XElement _user = new XElement("User");
-                XElement _fname = new XElement("FirestName");
-                _fname.Value = $"Имя {i}";
-                XElement _lname = new XElement("LasName");
-                _lname.Value = $"Фамилия {i}";
-                XAttribute _id = new XAttribute("Id", i);
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        XElement _user = new XElement("User");
+        //        XElement _fname = new XElement("FirestName");
+        //        _fname.Value = $"Имя {i}";
+        //        XElement _lname = new XElement("LasName");
+        //        _lname.Value = $"Фамилия {i}";
+        //        XAttribute _id = new XAttribute("Id", i);
 
-                _user.Add(_fname, _lname, _id);
-                _dep.Add(_user);
-            }
+        //        _user.Add(_fname, _lname, _id);
+        //        _dep.Add(_user);
+        //    }
 
-            _dep.Save("db.xml");
+        //    _dep.Save("db.xml");
 
-        }
+        //}
     }
 }
