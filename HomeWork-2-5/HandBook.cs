bfdb3a5ebd1649f17df7b72decba64c269e8e4dc
ganers <32878000+ganers@users.noreply.github.com>
2017-12-06 17:29:23 +0500

    2

---
+++ HomeWork-2-5/HandBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2_5
+{
+    class HandBook
+    {
+    }
+    /// <summary>
+    /// Справочник типов используемых СУБД
+    /// </summary>
+    public struct TypeDataBase
+    {
+        static public int FileSystem
+        {
+            get { return 0; }
+        }
+        static public int MySQL
+        {
+            get { return 1; }
+        }
+        static public int MSSQL
+        {
+            get { return 2; }
+        }
+        static public int PostgreSQL
+        {
+            get { return 3; }
+        }
+    }
+}
--- HomeWork-2-6/DataBase.cs
+++ HomeWork-2-6/DataBase.cs
@@ -7,60 +7,18 @@ using System.IO;
 
 namespace HomeWork_2_6
 {
-    public struct TypeDataBase
-    {
-        static public int FileSystem
-        {
-            get { return 0; }
-        }
-        static public int MySQL
-        {
-            get { return 1; }
-        }
-        static public int MSSQL
-        {
-            get { return 2; }
-        }
-        static public int PostgreSQL
-        {
-            get { return 3; }
-        }
-    }
-
     public class DataBase
     {
-        int typeDB;
         string nameDB;
+        List<Employee> employee;
+        List<Department> department;
 
-        public DataBase(int typeDB, string nameDB)
+        public DataBase(string nameDB)
         {
-            this.typeDB = typeDB;
             this.nameDB = nameDB;
         }
 
 
-        public Dictionary<string, string[]> ReadDataBase()
-        {
-            Dictionary<string, string[]> db = new Dictionary<string, string[]>();
 
-            if (this.typeDB == TypeDataBase.FileSystem)
-                db = ReadFileSystem(this.nameDB);
-            else if (this.typeDB == TypeDataBase.MySQL)
-                db = ReadMySQL();
-
-            return db;
-        }
-
-        Dictionary<string, string[]> ReadFileSystem(string path)
-        {
-
-            return File.ReadAllLines()
-        }
-
-        string[] ReadMySQL()
-        {
-            string[] db = new string[1];
-            return db;
-        }
     }
 }
